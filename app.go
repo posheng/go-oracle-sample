b59b935ad9f255d360a1cf5276de200ace8ca2f8
Samuel Huang <paisheng.h@gmail.com>
2018-11-16 16:57:17 +0800

    modify .gitignore file

--- app.go
+++ app.go
@@ -21,22 +21,25 @@ func main() {
 	} else {
 		glog.Info("Ping database success!")
 	}
-	//var dummy string
-	//if err := row.Scan(&dummy); err != nil {
-	//	log.Fatal(err)
-	//}
-	//if dummy != "1" {
-	//	log.Fatal("Failed to connect to database")
-	//}
-	var code string
-	var description string
-	row := db.QueryRow("SELECT CODE, DESCRIPTION FROM AC_REGION WHERE CODE = :1 AND DESCRIPTION = :2", "CN", "China")
-	if err := row.Scan(&code, &description); err != nil {
+	row := db.QueryRow("SELECT 1 FROM DUAL")
+	var dummy string
+	if err := row.Scan(&dummy); err != nil {
 		glog.Fatal(err)
 	}
-	glog.Info("Code:", code)
-	glog.Info("Description:", description)
-	glog.Info("Shutting Down")
+	if dummy != "1" {
+		glog.Fatal("Failed to connect to database")
+	} else {
+		glog.Info("Success to connect to database")
+	}
+	//var code string
+	//var description string
+	//row := db.QueryRow("SELECT CODE, DESCRIPTION FROM AC_REGION WHERE CODE = :1 AND DESCRIPTION = :2", "CN", "China")
+	//if err := row.Scan(&code, &description); err != nil {
+	//	glog.Fatal(err)
+	//}
+	//glog.Info("Code:", code)
+	//glog.Info("Description:", description)
+	//glog.Info("Shutting Down")
 	defer db.Close()
 	glog.Flush()
 }
